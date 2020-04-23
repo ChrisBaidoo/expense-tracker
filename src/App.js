3d9780827fa57b2066d94f50b7a2618d1c4f7615
christian baidoo <chris_pens@hotmail.co.uk>
2020-04-23 23:52:00 +0100

    Created the IncomeExpense and TransactionList Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div className="container">
       <Balance/>
       </div>
+      <IncomeExpenses/>
+      <TransactionList/>
     </div>
   );
 }
