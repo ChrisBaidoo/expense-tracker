35a07ee06793601ea9eba56b7452d45943943081
christian baidoo <chris_pens@hotmail.co.uk>
2020-04-27 22:58:19 +0100

    store user input in the state

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,14 +10,12 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmFor="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number"  placeholder="Enter amount..." />
+          <label htmFor="amount">Amount <br />
+            (negative - expense, positive - income)</label>
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
